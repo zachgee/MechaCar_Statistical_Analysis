895ec33c533d8e48587edbfe664521c6d05d688d
unknown <zgonzales16@gmail.com>
2022-02-06 19:43:52 -0600

    Challenge R script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,14 @@ Suspension_Coil_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsA
 total_summary <- Suspension_Coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') ##create summary of PSI column
 
 lot_summary <- Suspension_Coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') ##create lot summary df
+
+
+## Deliverable 3
+
+t.test((Suspension_Coil_df$PSI),mu=1500) ##T test to see if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+
+t.test((subset(Suspension_Coil_df, Manufacturing_Lot == "Lot1")$PSI),mu=(1500)) ##t test lot 1
+
+t.test((subset(Suspension_Coil_df, Manufacturing_Lot == "Lot2")$PSI),mu=(1500)) ##t test lot 2
+
+t.test((subset(Suspension_Coil_df, Manufacturing_Lot == "Lot3")$PSI),mu=(1500)) ##t test lot 3
