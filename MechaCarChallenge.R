66e520033fa8209c37c713b2389e00ecba330d2e
unknown <zgonzales16@gmail.com>
2022-02-06 18:48:47 -0600

    Deliverables 1 and 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,18 @@
 ##Deliverable 1
 
-library(dplyr) ##import dplyr 
+library(dplyr) ##import dplyr
+library(tidyverse)
 
 MechaCar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) ##read MechaCar_mpg.csv as a dataframe
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_df) ##perform linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_df)) ##perform summary to get p value and r squared value
+
+##Deliverable 2
+
+Suspension_Coil_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) ##read Suspension_Coil.csv as a dataframe
+
+total_summary <- Suspension_Coil_df %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep') ##create summary of PSI column
+
+lot_summary <- Suspension_Coil_df  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI),Std_Dev_PSI=sd(PSI),Num_Coil=n(), .groups = 'keep') ##create lot summary df
